4746fbfcd16ad3ad0f258aa3e5770aad90cfcffd
Glenn Vorhes <gavorhes@wisc.edu>
2016-10-04 15:23:12 -0500

    start conversion to new type declaration

--- lib/openlayers.d.ts
+++ lib/openlayers.d.ts
@@ -9,7 +9,7 @@ declare type GlobalObject = Object;
 /**
  * @namespace ol
  */
-declare module ol {
+export module ol {
     /**
      * The animation static methods are designed to be used with the
      * {@link ol.Map#beforeRender} method.  For example:
@@ -13783,7 +13783,7 @@ declare module olx {
     }
 
 }
-
-declare module "openlayers" {
-    export = ol;
-}
+//
+// declare module "openlayers" {
+//     export = ol;
+// }
