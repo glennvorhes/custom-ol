5f18716f39e554dcb95cfda94916f0a81d3cd550
Glenn Vorhes <gavorhes@wisc.edu>
2016-10-07 15:09:39 -0500

    finish conversion

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
 import {ol as _ol} from '../ol/ol-build';
-// export type GlobalObject = Object;
 export const ol = _ol;
-// export const olx = {};
 export default _ol;
-
-
-console.log(_ol);
-console.log(_ol);
-console.log(_ol);
-console.log(_ol);
-console.log(_ol);
-console.log(_ol);
-console.log(_ol);
-
-
