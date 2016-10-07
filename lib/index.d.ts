b6ce09b67c104062f942d4dc9337f27a6d04ef42
Glenn Vorhes <gavorhes@wisc.edu>
2016-10-07 13:52:26 -0500

    try namespace

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
 import { ol as _ol } from '../ol/ol-build';
 export declare const ol: typeof _ol;
+export default _ol;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import {ol as _ol} from './ol-mod';
-export declare type GlobalObject = Object;
+import {ol as _ol} from '../ol/ol-build';
+// export declare type GlobalObject = Object;
 export const ol = _ol;
-export default ol;
+export default _ol;
 
 
