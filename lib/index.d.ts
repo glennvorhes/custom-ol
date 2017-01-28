ecb62fdd3b1589a4d05167573a73c5c95dac3831
Glenn Vorhes <gavorhes@wisc.edu>
2017-01-27 18:30:05 -0600

    export ol

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,2 @@
 import { ol as _ol } from '../ol/ol-build';
 export declare const ol: typeof _ol;
-export default _ol;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import {ol as _ol} from '../ol/ol-build';
 export const ol = _ol;
-export default _ol;
+
